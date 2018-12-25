b8e5a54ed8c9eda31a84481cd27b0ca203c8ae91
snchildress <steven.n.childress@gmail.com>
2018-12-25 13:38:46 -0600

    Post ticket price to Slack

--- lambda.py
+++ lambda.py
@@ -10,6 +10,15 @@ def handler(event=None, context=None):
     response_body = response.json()
     lowest_price = response_body['stats']['lowest_price']
     print('SeatGeek Cheapest Ticket: $' + str(lowest_price))
+  
+  # post affordable ticket alert to Slack
+  request_url = config.SLACK_ENDPOINT
+  request_headers = {'Authorization': 'Bearer ' + config.SLACK_API_KEY}
+  request_body = {
+    'channel': config.SLACK_CHANNEL,
+    'text': 'SeatGeek Cheapest Ticket: $' + str(lowest_price)
+  }
+  response = requests.post(request_url, headers=request_headers, json=request_body)
 
 
 if config.ENV != 'prod' and '__main__' in __name__:
