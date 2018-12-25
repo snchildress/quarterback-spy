93e8588eff528d5c712ef074c7d81b0d0f950e6e
snchildress <steven.n.childress@gmail.com>
2018-12-24 23:10:35 -0600

    Add the SeatGeek API endpoint

--- config.py
+++ config.py
@@ -1,4 +1,9 @@
 import os
 
+
+# secrets
 ENV = os.environ.get('ENV', 'local')
 SEATGEEK_API_KEY = os.environ['SEATGEEK_API_KEY']
+
+# api endpoints
+SEATGEEK_ENDPOINT = 'https://api.seatgeek.com/2/events/{}?client_id=' + SEATGEEK_API_KEY
