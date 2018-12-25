5c58c1ddfda209e2f1a38c00c6008d3b8a11fca5
snchildress <steven.n.childress@gmail.com>
2018-12-24 23:17:39 -0600

    Add the SeatGeek NFC Championship event ID

--- config.py
+++ config.py
@@ -7,3 +7,6 @@ SEATGEEK_API_KEY = os.environ['SEATGEEK_API_KEY']
 
 # api endpoints
 SEATGEEK_ENDPOINT = 'https://api.seatgeek.com/2/events/{}?client_id=' + SEATGEEK_API_KEY
+
+# event ids for the NFC Championship game
+SEATGEEK_EVENT_ID = '4628040'
