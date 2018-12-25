a5137ed518c93513abecea1b1d0537f3f66f709c
snchildress <steven.n.childress@gmail.com>
2018-12-25 13:52:28 -0600

    Alert Slack if an offered price is below the max threshold

--- lambda.py
+++ lambda.py
@@ -11,19 +11,28 @@ def handler(event=None, context=None):
   response = requests.get(config.SEATGEEK_ENDPOINT.format(config.SEATGEEK_EVENT_ID))
   if response.ok:
     response_body = response.json()
-    lowest_prices['SeatGeek'] = response_body['stats']['lowest_price']
+    lowest_prices['SeatGeek'] = str(response_body['stats']['lowest_price'])
 
+  # iterate through the lowest prices to log and post affordable alert to Slack
   for marketplace, lowest_price in lowest_prices.items():
-    print(marketplace + ' Cheapest Ticket: $' + str(lowest_price))
+    # log each price in Slack
+    print(marketplace + ' Cheapest Ticket: $' + lowest_price)
+    if int(lowest_price) < config.MAX_AFFORDABLE_PRICE:
+      message = '@channel ' + marketplace + ' has an affordable ticket for $' + lowest_price + '!'
+      _message_slack(message)
 
-  # post affordable ticket alert to Slack
-  request_url = config.SLACK_ENDPOINT
-  request_headers = {'Authorization': 'Bearer ' + config.SLACK_API_KEY}
-  request_body = {
+def _message_slack(message):
+  """
+  Internal function to post a given message to the
+  configured Slack channel
+  """
+  url = config.SLACK_ENDPOINT
+  headers = {'Authorization': 'Bearer ' + config.SLACK_API_KEY}
+  body = {
     'channel': config.SLACK_CHANNEL,
-    'text': 'SeatGeek Cheapest Ticket: $' + str(lowest_price)
+    'text': str(message),
   }
-  response = requests.post(request_url, headers=request_headers, json=request_body)
+  requests.post(url, headers=headers, json=body)
 
 
 if config.ENV != 'prod' and '__main__' in __name__:
