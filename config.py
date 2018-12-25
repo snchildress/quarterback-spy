0187d016bf5a9e26e5333bb665b76a38238245c0
snchildress <steven.n.childress@gmail.com>
2018-12-25 13:34:49 -0600

    Set up Slack API config

--- config.py
+++ config.py
@@ -4,9 +4,14 @@ import os
 # secrets
 ENV = os.environ.get('ENV', 'local')
 SEATGEEK_API_KEY = os.environ['SEATGEEK_API_KEY']
+SLACK_API_KEY = os.environ['SLACK_API_KEY']
 
 # api endpoints
 SEATGEEK_ENDPOINT = 'https://api.seatgeek.com/2/events/{}?client_id=' + SEATGEEK_API_KEY
+SLACK_ENDPOINT = 'https://slack.com/api/chat.postMessage'
 
 # event ids for the NFC Championship game
 SEATGEEK_EVENT_ID = '4628040'
+
+# slack channel id
+SLACK_CHANNEL = os.environ.get('SLACK_CHANNEL', 'GF1BK650A')
